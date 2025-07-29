df192d3bdeb552f771803c3b7c7507c4a159555c
RuntyAxis1111 <johan.martinez@hybecorp.com>
2025-07-29 00:26:50 -0600

    Updated theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -4,4 +4,6 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
